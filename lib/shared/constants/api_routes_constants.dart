f9c6acd605d710160f785f6ae2105b567bc47f23
lvirrueta <luis510vg@gmail.com>
2024-11-09 14:32:48 -0600

    add api routes as constants

---
+++ lib/shared/constants/api_routes_constants.dart
@@ -0,0 +1,8 @@
+class ApiRoutes {
+  static String nowPlaying = 'movie/now_playing';
+  static String popular = 'movie/popular';
+  static String topRated = 'movie/top_rated';
+  static String upcoming = 'movie/upcoming';
+  static String detail = 'movie/detail';
+  static String search = 'search';
+}
